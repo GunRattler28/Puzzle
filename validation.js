992b61842baf85e1b5167b467819e2cff53cae07
GunRattler28 <aayushdeepchand@gmail.com>
2025-08-18 18:33:29 +0000

    Background Fix v3

--- validation.js
+++ validation.js
@@ -1,19 +1,10 @@
 const form = document.getElementById('form');
 const inputGroup = document.querySelector('.input-group');
 
-const backgrounds = ['images/Differently.jpg', 'images/lookCloser.png'];
-
-let passwordDone = 0;
-
-for (let i = 0; i < backgrounds.length; i++) {
-  const img = new Image();
-  img.src = backgrounds[i];
-}
-
-const layer1 = document.getElementById('bg1');
-const layer2 = document.getElementById('bg2');
+const layer1 = document.getElementById('link1');
+const layer2 = document.getElementById('link2');
 let activeLayer = 1;
-let currentBackgroundIndex = 0;
+let passwordDone = 0;
 
 layer1.style.opacity = 1;
 layer2.style.opacity = 0;
@@ -26,15 +17,12 @@ async function sha256(message) {
   return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
 }
 
-function changeBackground(newBackground) {
-  const fadeOutLayer = activeLayer === 1 ? layer1.parentElement : layer2.parentElement;
-  const fadeInLayer = activeLayer === 1 ? layer2.parentElement : layer1.parentElement;
-  const newHref = newBackground;
-  fadeInLayer.href = newHref;
-  fadeInLayer.querySelector('img').src = newBackground;
-  fadeOutLayer.style.opacity = 0;
-  fadeInLayer.style.opacity = 1;
-  activeLayer = activeLayer === 1 ? 2 : 1;
+function changeBackground(newLayer) {
+  const fadeOut = activeLayer === 1 ? layer1 : layer2;
+  const fadeIn = newLayer === 1 ? layer1 : layer2;
+  fadeOut.style.opacity = 0;
+  fadeIn.style.opacity = 1;
+  activeLayer = newLayer;
 }
 
 form.addEventListener('submit', async (e) => {
@@ -42,9 +30,9 @@ form.addEventListener('submit', async (e) => {
 
   const password = document.getElementById('password').value;
   const capitalPassword = password.split(' ')
-   .map(w => w[0].toUpperCase() + w.substring(1).toLowerCase())
-   .join(' ');
-  
+    .map(w => w[0].toUpperCase() + w.substring(1).toLowerCase())
+    .join(' ');
+
   const hashedPassword = await sha256(capitalPassword);
   const realPart1 = '7cb89be263253e2196661f303926cfb1bc662a607220486a69266dffb737af02';
   const realPart2 = '37766da024061aa60ba872415624f3bebd11d877d0fe098433c5948cccb7c6a8';
@@ -53,13 +41,13 @@ form.addEventListener('submit', async (e) => {
     alert('✅ Correct password.');
     inputGroup.classList.remove('incorrect');
     inputGroup.classList.add('correct');
-    changeBackground(backgrounds[1]);
+    changeBackground(2);
     passwordDone += 1;
   } else if ((hashedPassword === realPart2) && passwordDone === 1) {
     alert('✅ Correct password.');
     inputGroup.classList.remove('incorrect');
     inputGroup.classList.add('correct');
-    changeBackground(backgrounds[0]);
+    changeBackground(1);
   } else {
     alert('❌ Incorrect password.');
     inputGroup.classList.remove('correct');
