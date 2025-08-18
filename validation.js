4387328ff63e37d2d9df510062d87da2cc295857
GunRattler28 <aayushdeepchand@gmail.com>
2025-08-18 17:13:10 +0000

    Backgrounds Clickable and Fade

--- validation.js
+++ validation.js
@@ -3,20 +3,19 @@ const inputGroup = document.querySelector('.input-group');
 
 const backgrounds = ['images/Differently.jpg', 'images/lookCloser.png'];
 
+let passwordDone = 0;
+
 for (let i = 0; i < backgrounds.length; i++) {
   const img = new Image();
   img.src = backgrounds[i];
 }
-
 
 const layer1 = document.getElementById('bg1');
 const layer2 = document.getElementById('bg2');
 let activeLayer = 1;
 let currentBackgroundIndex = 0;
 
-layer1.style.backgroundImage = `url('${backgrounds[0]}')`;
 layer1.style.opacity = 1;
-layer2.style.backgroundImage = `url('${backgrounds[1]}')`;
 layer2.style.opacity = 0;
 
 async function sha256(message) {
@@ -28,9 +27,11 @@ async function sha256(message) {
 }
 
 function changeBackground(newBackground) {
-  const fadeOutLayer = activeLayer === 1 ? layer1 : layer2;
-  const fadeInLayer = activeLayer === 1 ? layer2 : layer1;
-  fadeInLayer.style.backgroundImage = `url('${newBackground}')`;
+  const fadeOutLayer = activeLayer === 1 ? layer1.parentElement : layer2.parentElement;
+  const fadeInLayer = activeLayer === 1 ? layer2.parentElement : layer1.parentElement;
+  const newHref = newBackground;
+  fadeInLayer.href = newHref;
+  fadeInLayer.querySelector('img').src = newBackground;
   fadeOutLayer.style.opacity = 0;
   fadeInLayer.style.opacity = 1;
   activeLayer = activeLayer === 1 ? 2 : 1;
@@ -47,8 +48,6 @@ form.addEventListener('submit', async (e) => {
   const hashedPassword = await sha256(capitalPassword);
   const realPart1 = '7cb89be263253e2196661f303926cfb1bc662a607220486a69266dffb737af02';
   const realPart2 = '37766da024061aa60ba872415624f3bebd11d877d0fe098433c5948cccb7c6a8';
-
-  let passwordDone = 0;
 
   if ((hashedPassword === realPart1) && passwordDone === 0) {
     alert('✅ Correct password.');
