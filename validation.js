78cfbf9d68a0553047584698b3db7e62bf520e82
GunRattler28 <aayushdeepchand@gmail.com>
2025-08-22 18:27:50 +0000

    Crossfade Not Working

--- validation.js
+++ validation.js
@@ -6,10 +6,6 @@ layer1.classList.add('active');
 let activeLayer = 1;
 let passwordDone = 0;
 
-layer1.style.opacity = 1;
-layer2.style.opacity = 0;
-layer1.classList.add('active');
-
 const realPasswords = [
   '7cb89be263253e2196661f303926cfb1bc662a607220486a69266dffb737af02',
   '37766da024061aa60ba872415624f3bebd11d877d0fe098433c5948cccb7c6a8'
@@ -27,6 +23,7 @@ function changeBackground(newLayer) {
   fadeOut.classList.remove('active');
   activeLayer = newLayer;
 }
+
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
   const password = document.getElementById('password').value.trim();
@@ -35,12 +32,10 @@ form.addEventListener('submit', async (e) => {
   const currentHash = realPasswords[passwordDone];
 
   if (hashedPassword === currentHash) {
-    alert('✅ Correct password.');
     inputGroup.className = 'input-group correct';
     changeBackground(passwordDone === 0 ? 2 : 1);
     passwordDone += 1;
   } else {
-    alert('❌ Incorrect password.');
     inputGroup.className = 'input-group incorrect';
   }
 });
