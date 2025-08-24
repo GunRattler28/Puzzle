847f59d92bd389938e426ce53427ef9a782f7e3b
GunRattler28 <aayushdeepchand@gmail.com>
2025-08-24 17:20:22 +0000

    Hash Updated

--- validation.js
+++ validation.js
@@ -1,9 +1,9 @@
 import { encryptPassword } from './encryption.js';
 
 const storedPasswords = [
-  { salt: "f1e2d3c4b5a60718aabbccddeeff0011", hash: "2b6f6b0df4c9a1f8e3d7e6b8f5a9c4d2e1f7a6b9c3d4e5f6a7b8c9d0e1f2a3b4" },
-  { salt: "2233445566778899aabbccddeeff0011", hash: "9c8b7a6d5e4f3b2a1d0c9e8f7a6b5c4d3e2f1a0b9c8d7e6f5a4b3c2d1e0f9a8b" },
-  { salt: "99aabbccddeeff001122334455667788", hash: "f1e2d3c4b5a60718293a4b5c6d7e8f09a1b2c3d4e5f60718293a4b5c6d7e8f0" }
+  { salt: "f1e2d3c4b5a60718aabbccddeeff0011", hash: "a950f729eb6bf4813aceb1f710dd164e59dfd9459243e2dd67b8111e60b41b1d" },
+  { salt: "2233445566778899aabbccddeeff0011", hash: "322014550cf4dc4563261f4bd4a325d2d93c41d1859fec32ac53d57587eb33ae" },
+  { salt: "99aabbccddeeff001122334455667788", hash: "68bdcf78b49b6433cc4acaa5bbce8a8269bf88f44c3189bb6ae3104f5e22bdc6" }
 ];
 
 const form = document.getElementById('form');
