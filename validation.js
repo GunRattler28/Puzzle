f810bd0b9ccd01be864f2ddd2f173d600360e79f
GunRattler28 <aayushdeepchand@gmail.com>
2025-08-26 14:39:43 +0000

    Puzzle Part 3 Password

--- validation.js
+++ validation.js
@@ -3,7 +3,7 @@ import { encryptPassword } from './encryption.js';
 const storedPasswords = [
   { salt: "f1e2d3c4b5a60718aabbccddeeff0011", hash: "a950f729eb6bf4813aceb1f710dd164e59dfd9459243e2dd67b8111e60b41b1d" },
   { salt: "2233445566778899aabbccddeeff0011", hash: "322014550cf4dc4563261f4bd4a325d2d93c41d1859fec32ac53d57587eb33ae" },
-  { salt: "99aabbccddeeff001122334455667788", hash: "68bdcf78b49b6433cc4acaa5bbce8a8269bf88f44c3189bb6ae3104f5e22bdc6" }
+  { salt: "99aabbccddeeff001122334455667788", hash: "a51684872d95e768205520eb0f91818c806e878bc28edb34b8176cd9691985dc" }
 ];
 
 const form = document.getElementById('form');
