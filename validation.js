354d77878d0628dc68321e4868d459a228751cc0
GunRattler28 <aayushdeepchand@gmail.com>
2025-09-02 07:13:58 +0000

    Part Setup 1

--- validation.js
+++ validation.js
@@ -3,7 +3,8 @@ import { encryptPassword } from './encryption.js';
 const storedPasswords = [
   { salt: "f1e2d3c4b5a60718aabbccddeeff0011", hash: "a950f729eb6bf4813aceb1f710dd164e59dfd9459243e2dd67b8111e60b41b1d" },
   { salt: "2233445566778899aabbccddeeff0011", hash: "322014550cf4dc4563261f4bd4a325d2d93c41d1859fec32ac53d57587eb33ae" },
-  { salt: "8e4882703df151aef59d718ea1cd3519", hash: "6321bf9a484de3d0698858c87c8d64bb4d67793b7734a1a970bfff7af0b47985" }
+  { salt: "8e4882703df151aef59d718ea1cd3519", hash: "6321bf9a484de3d0698858c87c8d64bb4d67793b7734a1a970bfff7af0b47985" },
+  { salt: "65ffffada8fb4923e3fafc4f4b42a247", hash: "d7c7dbd35443042b5c5aa4bb9174c8654392d202038ce246ba3606c2a291b4ae" }
 ];
 
 const form = document.getElementById('form');
@@ -38,8 +39,12 @@ form.addEventListener('submit', async (e) => {
     alert('Password correct! Proceeding to next step.');
     passwordStep += 1;
     changeBackground(passwordStep);
+    clear();
     if (passwordStep == 2) {
       console.log("s2UCIJ4_KAo");
+    } 
+    if (passwordStep == 3) {
+      console.log("\n 1. In wilds beyond they speak your name with reverence and regret,\nFor none could tame our ______ souls yet you the challenge met,\nUnder palest watch, you taught, we changed, base instincts were redeemed,\nA world you gave to bug and beast as they had never dreamed.\n");
     }
   } else {
     inputGroup.className = 'input-group incorrect';
