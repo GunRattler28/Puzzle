a7abaaf93593adc6a209d77d9fc9c9346cac760d
GunRattler28 <aayushdeepchand@gmail.com>
2025-09-02 09:35:10 +0000

    Part 4.3

--- validation.js
+++ validation.js
@@ -15,6 +15,7 @@ const layer3 = document.getElementById('link3');
 const layer4 = document.getElementById('link4');
 layer1.classList.add('active');
 const title = document.querySelector('h1');
+const codeBlock = document.getElementById('code');
 let passwordStep = 0;
 
 function changeBackground(step) {
@@ -46,10 +47,11 @@ form.addEventListener('submit', async (e) => {
     } 
     if (passwordStep == 3) {
       console.log("\n 1. In wilds beyond they speak your name with reverence and regret,\nFor none could tame our ______ souls yet you the challenge met,\nUnder palest watch, you taught, we changed, base instincts were redeemed,\nA world you gave to bug and beast as they had never dreamed.\n");
+      codeBlock.textContent = "He stands where currents twist and decay lingers, once a loyal sentinel of old. The name he first bore is rarely spoken aloud. Utter it to continue.";
       title.style.cursor = 'pointer';
       title.addEventListener('click', () => {
       alert("4. So you'd pursue the deeper truth? It isn't one the weak could bear. Prove yourself ready to face it. I'll not hold back. My _____ is lethal and I'd feel no sadness in a weakling's demise. Show me you can accept this Kingdom's past and claim responsibility for its future.");
-  });
+      });
     }
   } else {
     inputGroup.className = 'input-group incorrect';
